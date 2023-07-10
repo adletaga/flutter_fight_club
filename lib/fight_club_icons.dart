232ccbac8d24d8397d3af9fa4b82f110b6b4f10d
adletaga <asdqrrwqFFF412@>
2023-07-10 22:27:59 +0600

    added background of players

--- lib/fight_club_icons.dart
+++ lib/fight_club_icons.dart
@@ -3,4 +3,6 @@ class FightClubIcons {
   FightClubIcons._();
   static const heartFull = "assets/icons/heart-full.png";
   static const heartEmpty = "assets/icons/heart-empty.png";
+  static const youAvatar = "assets/icons/you-avatar.png";
+  static const enemyAvatar = "assets/icons/enemy-avatar.png";
 }
